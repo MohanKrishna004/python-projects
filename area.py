e9c9c1ac903379a96f6bc7229fd42a84250739a0
MohanKrishna004 <130684960+MohanKrishna004@users.noreply.github.com>
2023-04-13 21:23:22 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("hello")
+pi=3.14
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
