4e847f662627b0e1e3615da7b9114c73907d0de8
MohanKrishna004 <130684960+MohanKrishna004@users.noreply.github.com>
2023-04-13 21:24:04 +0530

    Add files via upload

---
+++ exten.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("hello")
+filename=input("enter the file name:")
+f_extns=filename.split(".")
+print("the extension is:"+repr(f_extns[-1]))
